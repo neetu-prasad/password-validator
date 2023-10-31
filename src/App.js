96ec626700df174d0924d402bdd3e830c65cfa65
Neetu Prasad <neetumehta510@gmail.com>
2023-10-31 23:35:27 +0530

    react setup done and form built for password validation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PasswordForm from './components/PasswordForm';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordForm />
+    </div>
+  );
+}
+
+export default App;
